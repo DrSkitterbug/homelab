ffc9720d06b46ace28ed7b8a77e429d894531a2d
Exscallion <176167962+Exscallion@users.noreply.github.com>
2026-01-04 02:23:51 +0100

    [feat] Add home manager

--- workstation/flake.nix
+++ workstation/flake.nix
@@ -3,17 +3,23 @@
   
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86-64-linux";
-    in {
-      nixosConfigurations.palica = nixpkgs.lib.nixosSystem {
-        inherit system;
+  outputs = { self, nixpkgs, home-manager, ... } @inputs: {
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.nixosSystem {
         modules = [
           ./configuration.nix
+          home-manager.nixosModules.home-manager 
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.zeta = ./home.nix;
+          }
         ];
       };
     };
+  };
 }
